8d74a02cd4d36093019f5797b5e1c119271ef674
Anirudh <anirudh.formal88@gmail.com>
2026-07-19 23:20:24 +0530

    API Calling using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
+
+  const getProductData = async () => {
+    try {
+      let res = await axios.get('https://dummyjson.com/products')
+      console.log("API Response → ", res)
+    }
+    catch (error) {
+      console.log("Error in API → ", error)
+    }
+  }
+
+  getProductData()
   
   return (
-    <div></div>
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
   )
 }
 
